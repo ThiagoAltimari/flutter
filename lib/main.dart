ec5a3285f438483ce524c94917f9f0a84929060b
Buttslide <tsmaaltimari@gmail.com>
2020-02-27 10:27:36 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+        home: Container(
+      color: Colors.red,
+      child: Center(
+        child: Text(
+          "",
+          style: TextStyle(color: Colors.white),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    )));
